781f6eec524c29dc29df0a11531944de80be5596
Shawn Erquhart <shawn@professant.com>
2016-08-20 16:47:11 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const _ = require('lodash');
+const argv = require('yargs').argv;
+const fs = require('fs-extra');
+const path = require('path');
+const http = require('http');
+const open = require('open');
+const ecstatic = require('ecstatic');
+const contentful = require('contentful');
+
+const config = fs.readJsonSync(path.join(process.cwd(), 'millwright.json'));
+const cleanDirs = ['dest'];
+const scriptsDir = path.join(__dirname, 'lib');
+const contentfulKeys = config.contentful;
+const serveRoot = 'dest';
+const servePort = 8080;
+const servePath = 'http://localhost:8080'
+const serveMsg = 'Millwright serving at ' + servePath + '...';
+const defaultCommand = 'dev';
+
+const mill = {
+  templateDeps: requireBuildScript('template-deps'),
+  parseContent: requireBuildScript('parse-content'),
+  pages: requireBuildScript('pages'),
+  clean,
+  build,
+  dev,
+  serve
+};
+
+const cmd = argv._[0];
+
+module.exports = runMill;
+
+function runMill(cmd) {
+  if (!cmd) {
+    mill[defaultCommand]();
+  } else if (_.isString(cmd) && mill[cmd]) {
+    mill[cmd]();
+  } else if (_.isString(cmd)) {
+    console.log('mill: "' + cmd + '" is not a recognized command.');
+  }
+}
+
+function clean() {
+  cleanDirs.forEach(dir => fs.removeSync(dir));
+}
+
+function build() {
+  return contentful.createClient(contentfulKeys).getEntries().then(entries => {
+    return mill.pages(_.assign(mill.templateDeps(), mill.parseContent(entries.items)));
+  });
+}
+
+function dev() {
+  mill.build().then(() => mill.serve());
+}
+
+function serve() {
+  http.createServer(ecstatic(serveRoot)).listen(servePort);
+  console.log(serveMsg);
+  open(servePath);
+}
+
+function requireBuildScript(scriptName) {
+  return require(path.resolve(scriptsDir, scriptName + '.js'));
+}
